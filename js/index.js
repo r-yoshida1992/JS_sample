6bcfe1e780c058110080bb6be521a85d48234d1d
r-yoshida1992 <yoshida@link-ps.co.jp>
2021-02-19 00:35:03 +0900

    ワードを一つ打ったら次のワードが出るように改修

--- js/index.js
+++ js/index.js
@@ -1,9 +1,5 @@
-const WORDS = {
-    kanji: '東京特許許可局',
-    furigana: 'とうきょうとっきょきょかきょく',
-    inputWord: 'TOUKYOUTOKKYOKYOKAKYOKU',
-}
-
+var i = 0;
+var randomIndex = Array();
 
 window.onload = () => {
     initApp();
@@ -21,28 +17,41 @@ function initApp() {
     const word = create('div', 'word-text')
     const enterdSpan = create('span', 'enterd');
     const notEnterdSpan = create('span', 'not-enterd');
+    const hiddenText = create('span', 'hidden-text');
 
     // contains build
     appRoute.append(container);
     container.append(wordHead);
     container.append(wordFurigana);
     container.append(word);
+    container.append(hiddenText);
     word.append(enterdSpan);
     word.append(notEnterdSpan);
+    hiddenText.setAttribute('type', 'hidden');
 
-    wordHead.textContent = WORDS.kanji;
-    wordFurigana.textContent = WORDS.furigana;
-    notEnterdSpan.textContent = WORDS.inputWord;
+    for (let i = 0; i < wordArray.length; i++) {
+        randomIndex.push(i);
+    }
+    randomIndex = shuffle(randomIndex);
+
+    setWord();
 }
 
 function keyDownAction() {
+    const hiddenText = document.getElementById('hidden-text');
+
     const notEnterd = document.getElementById('not-enterd');
     const enterd = document.getElementById('enterd');
     document.addEventListener('keydown', (event) => {
         if (event.key.toUpperCase() === notEnterd.textContent.charAt(0)) {
             enterd.textContent += notEnterd.textContent.charAt(0);
             notEnterd.textContent = notEnterd.textContent.slice(1);
+
+            if (enterd.textContent === hiddenText.textContent) {
+                setWord();
+            }
         }
+
     })
 }
 
@@ -50,4 +59,36 @@ function create(tagName, idName) {
     const element = document.createElement(tagName);
     element.id = idName;
     return element;
+}
+
+function setWord() {
+    const wordHead = document.getElementById('word-head');
+    const wordFurigana = document.getElementById('word-furigana');
+    const hiddenText = document.getElementById('hidden-text');
+    const notEnterd = document.getElementById('not-enterd');
+    const enterd = document.getElementById('enterd');
+
+    if (i < 5) {
+        wordHead.textContent = wordArray[randomIndex[i]].kanji;
+        wordFurigana.textContent = wordArray[randomIndex[i]].furigana;
+        notEnterd.textContent = wordArray[randomIndex[i]].inputWord;
+        enterd.textContent = '';
+        hiddenText.textContent = wordArray[randomIndex[i]].inputWord;
+        i++;
+    } else {
+        wordHead.textContent = '';
+        wordFurigana.textContent = '';
+        notEnterd.textContent = '';
+        enterd.textContent = '';
+        hiddenText.textContent = '';
+        setTimeout(() => { alert('終了!!') }, 10);
+    }
+}
+
+function shuffle(array) {
+    for (let i = array.length - 1; i >= 0; i--) {
+        let rand = Math.floor(Math.random() * (i + 1));
+        [array[i], array[rand]] = [array[rand], array[i]]
+    }
+    return array;
 }
---
+++ js/words.js
@@ -0,0 +1,62 @@
+const wordArray = [
+    {
+        kanji: '東京特許許可局',
+        furigana: 'とうきょうとっきょきょかきょく',
+        inputWord: 'TOUKYOUTOKKYOKYOKAKYOKU',
+    },
+    {
+        kanji: 'プログラミング言語',
+        furigana: 'ぷろぐらみんぐげんご',
+        inputWord: 'PUROGURAMINNGUGENGO',
+    },
+    {
+        kanji: '犬も歩けば棒に当たる',
+        furigana: 'いぬもあるけばぼうにあたる',
+        inputWord: 'INUMOARUKEBABOUNIATARU',
+    },
+    {
+        kanji: 'スタバなう',
+        furigana: 'すたばなう',
+        inputWord: 'SUTABANAU',
+    },
+    {
+        kanji: 'ラーメン食べたい',
+        furigana: 'らーめんたべたい',
+        inputWord: 'RA-MENNTABETAI',
+    },
+    {
+        kanji: '寿司',
+        furigana: 'すし',
+        inputWord: 'SUSHI',
+    },
+    {
+        kanji: '牛',
+        furigana: 'うし',
+        inputWord: 'USHI',
+    },
+    {
+        kanji: 'ケーキ',
+        furigana: 'けーき',
+        inputWord: 'KE-KI',
+    },
+    {
+        kanji: 'お前はもう死んでいる',
+        furigana: 'おまえはもうしんでいる',
+        inputWord: 'OMAEHAMOUSHINNDEIRU',
+    },
+    {
+        kanji: 'キュンです',
+        furigana: 'きゅんです',
+        inputWord: 'KYUNNDESU',
+    },
+    {
+        kanji: '馬鹿',
+        furigana: 'ばか',
+        inputWord: 'BAKA',
+    },
+    {
+        kanji: '努力不足',
+        furigana: 'どりょくぶそく',
+        inputWord: 'DORYOKUBUSOKU',
+    }
+]
